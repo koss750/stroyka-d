d5d034297caf8182ef100a29b3895391ad5868a3
Konstantin Borodin <web@borodin.uk>
2023-12-29 23:01:37 +0000

    added command to try update descriptions with AI

--- app/Console/Commands/UpdateDesignDetails.php
+++ app/Console/Commands/UpdateDesignDetails.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Design;
+use Illuminate\Support\Facades\Http;
 
 class UpdateDesignDetails extends Command
 {
@@ -11,20 +13,48 @@ class UpdateDesignDetails extends Command
      *
      * @var string
      */
-    protected $signature = 'app:update-design-details';
+    protected $signature = 'design:details';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Use AI to update descriptions';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $designs = Design::all();
+
+        foreach ($designs as $design) {
+            // Check if 'details' is empty and 'image_url' is set
+            if (empty($design->details) && $design->setImage()) {
+                $prompt = "Please give me a description of around 100-150 characters about this house plan. It looks like {$design->image_url}, it's area is {$design->size} and here is a description of all its rooms {$design->floorsList}";
+
+                $response = Http::withHeaders([
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer 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'
+                ])->post('https://api.openai.com/v1/chat/completions', [
+                    'model' => 'gpt-3.5-turbo',
+                    'messages' => [
+                        ["role" => "user", "content" => $prompt]
+                    ],
+                    'temperature' => 0.7
+                ]);
+
+                if ($response->successful()) {
+                    $design->details = $response->json()['choices'][0]['message']['content'];
+                    $design->save();
+                    $this->info("Updated details for design ID {$design->id}");
+                } else {
+                    $this->error("Failed to update design ID {$design->id}");
+                }
+            }
+        }
+
+        $this->info('Design details update complete.');
     }
 }
